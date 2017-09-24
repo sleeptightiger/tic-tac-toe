9d2ba4da9ad970e254d26908a344f1a4febb0114
Gerry Morales Meza <sleep.tiger2@gmail.com>
2017-09-24 18:25:52 -0500

    fix check3inRow

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
 
-//fix diagonal inverse diagonal check
-//displayWinner not working
 
-//creat board
+
+
 
 let row = ['A', 'B', 'C'];
 let column = ['0', '1', '2'];
@@ -48,7 +47,7 @@ function populateBoard() {
             board[i][j] = box;
             let thisBox = board[i][j];
             let boxElement = document.querySelector(`.${row[i]}${column[j]}`);
-            console.log('clicked is NOT running..');
+
             boxElement.addEventListener('click', function(event){
 
 
@@ -64,7 +63,7 @@ function populateBoard() {
 
 
                     displayBoard(board);
-                    //console.log(`whosTurn: ${whosTurn}, gameOver: ${gameOver}`);
+
 
 
 
@@ -83,8 +82,7 @@ function populateBoard() {
 
 // display who's turn
 function turn(justChecking) {
-    //console.log('turn() is running..');
-    //console.log(`whosTurn: ${whosTurn}, gameOver: ${gameOver}`);
+
     if(justChecking) {
         //rewrite as ternary shorthand
         if(whosTurn) {
@@ -93,23 +91,21 @@ function turn(justChecking) {
             return 'O';
         }
     } else {
-        // console.log('TURN SWITCHED!!!!!!!!!!!!!!!');
+
         whosTurn = !whosTurn;
     }
 
 }
 
 function displayWhosMove() {
-    //console.log('displayWhosMove() is running..');
-    //console.log(`whosTurn: ${whosTurn}, gameOver: ${gameOver}`);
+
     let heading = document.querySelector('.displayTurn');
     let t = turn(true);
     heading.innerText = `Player ${t}\'s move`;
 }
 
 function isBoardFull(board) {
-    //console.log('isBoardFull() is running..');
-    //console.log(`whosTurn: ${whosTurn}, gameOver: ${gameOver}`);
+
     let check = true;
     for (var i = 0; i < board.length; i++) {
         for (var j = 0; j < board[i].length; j++) {
@@ -123,9 +119,8 @@ function isBoardFull(board) {
 }
 
 // check if three in a row
-function check3inRow(board) {
-    console.log('check3inRow() is running..');
-    console.log(`whosTurn: ${whosTurn}, gameOver: ${gameOver}, turn(true): ${turn(true)}`);
+function check3inRow(board, turn) {
+
     let check = false;
 
     let count = 0;
@@ -137,22 +132,23 @@ function check3inRow(board) {
     for (var i = 0; i < board.length; i++) {
         for (var j = 0; j < board[i].length; j++) {
             //check row
-            if(board[i][j].mark === turn(true)) {
+            if(board[i][j].mark === turn) {
                 count++;
             }
             //check column
-            if (board[j][i].mark === turn(true)) {
+            if (board[j][i].mark === turn) {
                 columnCount++;
             }
             //check diagonal
-            if(i === j && board[i][j].mark === turn(true)) {
+            if(i === j && board[i][j].mark === turn) {
                 dCount++;
             }
             //check inverse diagonal
-            if((i === j || (i === 0 && j === 2) || (i === 2 && j === 0)) && board[i][j].mark === turn(true)) {
+            if(((i === 1 && j === 1) || (i === 0 && j === 2) || (i === 2 && j === 0)) && board[i][j].mark === turn) {
                 invDCount++;
             }
         }
+        console.log(`dCount: ${dCount}, invDCount: ${invDCount}`);
         if(count === 3 || columnCount === 3 || dCount === 3 || invDCount === 3) {
             check = true;
             break;
@@ -166,26 +162,24 @@ function check3inRow(board) {
 }
 
 
-function displayWinner() {
+function displayWinner(turn) {
     console.log('displayWinner() is running..');
-    //console.log(`whosTurn: ${whosTurn}, gameOver: ${gameOver}`);
+
     let heading = document.querySelector('.displayTurn');
-    let t = turn(true);
-    heading.innerText = `Player ${t} wins!!`;
+
+    heading.innerText = `Player ${turn} wins!!`;
     gameOver = true;
 }
 
 function displayDraw() {
-    //console.log('displayDraw() is running..');
-    //console.log(`whosTurn: ${whosTurn}, gameOver: ${gameOver}`);
+
     let heading = document.querySelector('.displayTurn');
     heading.innerText = `This game is a draw ¯\\_(ツ)_/¯`;
     gameOver = true;
 }
 
 function displayBoard(board) {
-    // console.log('displayBoard() is running..');
-    // console.log(`whosTurn: ${whosTurn}, gameOver: ${gameOver}, isFirstMove: ${isFirstMove}`);
+
     for (var i = 0; i < board.length; i++) {
         for (var j = 0; j < board[i].length; j++) {
             let thisBox = board[i][j];
@@ -199,26 +193,27 @@ function displayBoard(board) {
         }
     }
 
-
-
-
-
-    if(check3inRow(board)) {
-        displayWinner();
-    }
-
-    if(isBoardFull(board)) {
-        displayDraw();
-    }
-
-
+    let checkForThisTurn = turn(true);
     if(!isFirstMove) {
-        //console.log("turn about to be switched!!!!!!!!!!!!!!");
+
         turn();
     }
     isFirstMove = false;
 
     displayWhosMove();
+
+    if(isBoardFull(board)) {
+        displayDraw();
+    }
+
+    if(check3inRow(board, checkForThisTurn)) {
+        displayWinner(checkForThisTurn);
+    }
+
+
+
+
+
 
 }
 
@@ -226,14 +221,13 @@ function displayBoard(board) {
 
 
 function reset() {
-    //console.log('rest() is running..');
-    //console.log(`whosTurn: ${whosTurn}, gameOver: ${gameOver}`);
+
     clearBoard();
 
 
     whosTurn = true;
     gameOver = false;
-    notGameStart = false;
+    isFirstMove = true;
     createBoard();
     let nextGame = populateBoard();
     displayBoard(nextGame);
@@ -241,8 +235,7 @@ function reset() {
 }
 
 function clearBoard() {
-    //console.log('clearBoard() is running..');
-    //console.log(`whosTurn: ${whosTurn}, gameOver: ${gameOver}`);
+
     let board = document.querySelector('.board');
     board.innerHTML = '';
 }
